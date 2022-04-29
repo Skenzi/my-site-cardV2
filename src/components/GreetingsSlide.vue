9df49ce24b114d77d4667f69162284eeb375bdff
Skenzi <dedooman@mail.ru>
2022-04-29 20:36:11 +0500

    add new components

---
+++ src/components/GreetingsSlide.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="slide">
+        <h1 class="slide__title">Privet</h1>
+        <p class="slide__greetings">Zaxodi</p>
+    </div>
+</template>
+<script>
+export default {
+    setup() {
+        
+    },
+}
+</script>
