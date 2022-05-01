999db6234f04fb3e9749e22891d09104e16b7931
root <root@DESKTOP-22MPB7K.localdomain>
2022-05-01 22:40:08 +0500

    update Menu and add images

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,10 +1,15 @@
 <template>
     <nav class="navigation">
-        <ButtonLink class="navigation__link" color="green" pathTo="/" text="Home" />
-        <button class="navigation__button" v-on:click="setActiveId('Bio')">БИО</button>
-        <button class="navigation__button" v-on:click="setActiveId('Path to IT')">Путь в IT</button>
-        <button class="navigation__button" v-on:click="setActiveId('Abilities')">Навыки и технологии</button>
-        <button class="navigation__button" v-on:click="setActiveId('Contacts')">Контакты</button>
+        <button class="navigation__button-fold"></button>
+        <div class="navigation__wrapper">
+            <ButtonLink class="navigation__link" color="green" pathTo="/" text="Home" />
+            <div class="navigation__menu">
+                <button class="menu__button" v-on:click="setActiveId('Bio')">БИО</button>
+                <button class="menu__button" v-on:click="setActiveId('Path to IT')">Путь в IT</button>
+                <button class="menu__button" v-on:click="setActiveId('Abilities')">Навыки и технологии</button>
+                <button class="menu__button" v-on:click="setActiveId('Contacts')">Контакты</button>
+            </div>
+        </div>
     </nav>
 </template>
 <script>
@@ -20,9 +25,23 @@ export default {
 </script>
 <style>
 .navigation {
-
+    padding: 20px;
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    left: 0;
 }
-.navigation__button {
+.navigation__wrapper {
+    display: flex;
+}
+.navigation__button-fold {
+    display: block;
+    margin: 0 auto;
+    padding: 0;
+    border: 20px solid white;
+    border-bottom: 20px solid black;
+}
+.navigation .menu__button {
     padding: 20px 40px;
     border: none;
     background-color: transparent;
