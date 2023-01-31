440dd9109d8c42d8547de34625ee5c33b69c79af
Skenzi <dedooman@mail.ru>
2023-01-31 18:50:34 +0500

    update slides Bio and Abilities

--- src/components/SlideAbilities.vue
+++ src/components/SlideAbilities.vue
@@ -3,44 +3,49 @@
         <h2 :class=slideTitleClasses class="relitive left-0">Навыки</h2>
         <div :class=slideTextClasses class="container-f relitive left-0">
             <section class="slide-abilities-section">
-            <h3>Языки программирования</h3>
-            <ul>
-            <li>
-                JS(ES5+)
-            </li>
-            <li>
-                PHP
-            </li>
-        </ul>
-        </section>
-        <section class="slide-abilities-section">
-            <h3>Фреймворки, библиотеки</h3>
-            <ul>
-            <li>
-                Redux
-            </li>
-            <li>
-                React
-            </li>
-            <li>
-                Vue
-            </li>
-            <li>
-                SCSS
-            </li>
-        </ul>
-        </section>
-        <section class="slide-abilities-section">
-            <h3>Верстка, стили</h3>
-            <ul>
-            <li>
-                HTML5
-            </li>
-            <li>
-                CSS3
-            </li>
-        </ul>
-        </section>
+                <h3>Языки программирования</h3>
+                <ul>
+                    <li>
+                        JS(ES5+)
+                    </li>
+
+                    <li>
+                        Python
+                    </li>
+
+                    <li>
+                        PHP
+                    </li>
+                </ul>
+            </section>
+            <section class="slide-abilities-section">
+                <h3>Фреймворки, библиотеки</h3>
+                <ul>
+                    <li>
+                        Redux
+                    </li>
+                    <li>
+                        React
+                    </li>
+                    <li>
+                        Vue
+                    </li>
+                </ul>
+            </section>
+            <section class="slide-abilities-section">
+                <h3>Верстка, стили</h3>
+                <ul>
+                    <li>
+                        HTML5
+                    </li>
+                    <li>
+                        CSS3
+                    </li>
+                    <li>
+                        SCSS
+                    </li>
+                </ul>
+            </section>
         </div>
     </div>
 </template>
@@ -50,13 +55,13 @@ export default {
     props: ['isChangeSlide'],
     name: 'slide-abilities',
     computed: {
-        slideTitleClasses: function() {
+        slideTitleClasses: function () {
             return {
                 'slide__title--toLeft': this.isChangeSlide,
                 'slide__title--fromLeft': !this.isChangeSlide,
             }
         },
-        slideTextClasses: function() {
+        slideTextClasses: function () {
             return {
                 'slide__body--toLeft': this.isChangeSlide,
                 'slide__body--fromLeft': !this.isChangeSlide,
@@ -66,14 +71,15 @@ export default {
 }
 </script>
 <style>
-    .slide {
-       height: 60vh;
-       display: flex;
-       flex-direction: column;
-       justify-content: center;
-       padding: 20px;
-    }
-    .slide-abilities-section {
-        flex: 1;
-    }
+.slide {
+    height: 60vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 20px;
+}
+
+.slide-abilities-section {
+    flex: 1;
+}
 </style>
